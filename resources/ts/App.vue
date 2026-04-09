6c694443bdbdc133528c59f88ec9db87970ecc92
MRootxx <dr.rootvb@gmail.com>
2026-04-09 14:07:00 +0300

    fix: remove TypeScript type assertion from Vue template in App.vue (rolldown parse error)

--- resources/ts/App.vue
+++ resources/ts/App.vue
@@ -1,7 +1,7 @@
 <template>
   <VApp>
     <RouterView v-slot="{ Component, route }">
-      <Transition :name="route.meta.transition as string || 'page-fade'" mode="out-in">
+      <Transition :name="(route.meta.transition as string) || 'page-fade'" mode="out-in">
         <component :is="Component" :key="route.path" />
       </Transition>
     </RouterView>
